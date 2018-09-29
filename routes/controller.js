87f602bb1370ed16d4b06b8605e704fc9fcd1bc8
unknown <pmd7211@gmail.com>
2018-09-29 17:46:22 -0400

    my project

--- routes/controller.js
+++ routes/controller.js
@@ -1,12 +1,49 @@
 var express = require('express');
 var router = express.Router();
-var Profile = require('../models').profile
-//var Profile = require('../mock/profile')
+//var Profile = require('../models').profile
+var Profile = require('../mock/profile')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.sendStatus(200)
+  res.send('Hello World')
 });
+
+router.get('/profiles', function (req, res) {
+  res.send(Profile.findAll())
+})
+
+router.get('/profiles/:id', function (req, res) {
+  var profile = Profile.findById(req.params.id)
+  res.send(profile)
+})
+
+router.post('/profiles', function (req, res) {
+  var profiles = Profile.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    bio: req.body.bio,
+    interests: req.body.interests,
+    skills: req.body.skills
+  })
+  res.send(profiles)
+})
+
+router.put('/profiles/:id', function(req, res) {
+  var profile = Profile.update(req.params.id, {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    bio: req.body.bio,
+    interests: req.body.interests,
+    skills: req.body.skills
+  })
+  res.send(profile)
+})
+
+router.delete('/profiles/:id', function(req, res) {
+  var profiles = Profile.remove(req.params.id)
+  res.send(profiles)
+})
+
 
  
 module.exports = router;
